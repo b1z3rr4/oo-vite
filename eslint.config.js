a2389a8114d71f0334053b193b1461400be10d03
Natalia Bezerra <b1z3rr4@gmail.com>
2024-09-05 23:10:41 -0300

    configuracao de projeto

--- eslint.config.js
+++ eslint.config.js
@@ -1,22 +1,31 @@
-import { defineConfig } from 'eslint-define-config';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import prettierConfig from 'eslint-config-prettier';
+import prettierPlugin from 'eslint-plugin-prettier';
 
-export default defineConfig({
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 2020,
-    sourceType: 'module',
+/** @type {import('eslint').Linter.FlatConfig[]} */
+export default [
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+      prettier: prettierPlugin,
+    },
+    rules: {
+      ...tsPlugin.configs.recommended.rules,
+      ...prettierConfig.rules,
+      semi: ['error', 'always'],
+      'no-unused-vars': 'warn',
+      'no-console': 'warn',
+      'prefer-const': 'error',
+      '@typescript-eslint/no-unused-vars': ['error'],
+    },
   },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'prettier',
-  ],
-  plugins: ['@typescript-eslint'],
-  rules: {
-    semi: ['error', 'always'],
-    'no-unused-vars': 'warn',
-    'no-console': 'warn',
-    'prefer-const': 'error',
-    '@typescript-eslint/no-unused-vars': ['error'],
-  },
-});
+];
